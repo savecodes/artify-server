2df1b39965005556e51786c03853bfada875bd92
savecodes <codedevs7@gmail.com>
2025-12-06 14:42:26 +0600

    connect to mongodb and create collections

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-
-const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Welcome to Artify Server");
 });
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
